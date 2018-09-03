e702dc84742087551be1672f81022c51d470598a
Doug Schaefer <schae@uct3-s1.mwt2.org>
2018-09-03 06:55:10 -0500

    =add samples

--- VBFAnalysis/python/sample.py
+++ VBFAnalysis/python/sample.py
@@ -43,6 +43,8 @@ class sample(object):
                 self.sampleType = "W_strong"
             elif ((self.runNumber >= 308092 and self.runNumber <= 308095) or (self.runNumber >= 363355 and self.runNumber <= 363358)):
                 self.sampleType = "Z_EWK"
+            elif (self.runNumber >= 345099 and self.runNumber <= 345102):
+                self.sampleType = "Z_VBFFilt"
             elif (self.runNumber >= 364100 and self.runNumber <= 364155):
                 self.sampleType = "Z_strong"
             elif ((self.runNumber >= 410011 and self.runNumber <= 410014) or (self.runNumber == 410025) or (self.runNumber == 410026) or (self.runNumber == 410470) or (self.runNumber == 410471)):
@@ -53,6 +55,16 @@ class sample(object):
                 self.sampleType = "ggFH125"
             elif ((self.runNumber == 308071) or (self.runNumber == 308072)):
                 self.sampleType = "VH125"
+            elif (self.runNumber >= 361020 and self.runNumber <= 361032):
+                self.sampleType = "QCDw"
+            elif (self.runNumber >= 426001 and self.runNumber <= 426009):
+                self.sampleType = "QCDunw"
+            elif (self.runNumber == 363494 or self.runNumber == 364250 or self.runNumber == 364254 or self.runNumber == 364255):
+                self.sampleType = "VV"
+            elif (self.runNumber >= 364242 and self.runNumber <= 364249):
+                self.sampleType = "VVV"
+            elif (self.runNumber >= 364198 and self.runNumber <= 364215):
+                self.sampleType = "Z_strong_LowMass"
             else:
                 print "python/sample.py: runNumber "+str(self.runNumber)+" could not be identified as a valid MC :o"
                 self.sampleType = "ERROR"
