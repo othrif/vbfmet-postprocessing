15e8495c7b2f1088e3eeebabbcbbc3ffe87cd577
Doug Schaefer <schae@uct3-s1.mwt2.org>
2018-09-09 06:32:52 -0500

    =add the signal and qcd as separate files

--- VBFAnalysis/share/HFInputJobOptions.py
+++ VBFAnalysis/share/HFInputJobOptions.py
@@ -12,6 +12,7 @@ config = JobConfigurationBase("JobOptions")
 arg_group = config.parser.add_argument_group("JobOptions", "Extra arguments specific to HFInputJobOptions")
 arg_group.add_argument("--currentVariation", dest='currentVariation', default="Nominal", help="current systematics, default: NONE")
 arg_group.add_argument("--containerName", dest='containerName', default="", help="container name used to look up the sample ID if not in the file path")
+arg_group.add_argument("--currentSample", dest='currentSample', default="", help="sample name to process")
 arg_group.add_argument("--doLowNom", action="store_true", dest='doLowNom', default=False, help="doLowNom, to symmetrize asymmetric syst, default: False")
 arg_group.add_argument("--isLow", action="store_true", dest='isLow', default=False, help="isLow, default: False")
 arg_group.add_argument("--oldInput", action="store_true", dest='oldInput', default=False, help="if oldInput, switch to HFInputOldInput")
@@ -23,12 +24,18 @@ if (args.doLowNom):
     doLowNom_str = "Low"
 inputDir = str(jps.AthenaCommonFlags.FilesInput)
 s = VBFAnalysis.sample.sample("", args.currentVariation)
-currentSample=''
+currentSampleKey=''
+currentSampleList=[]
+if args.currentSample!="":
+    currentSampleList+=[args.currentSample]
 subfileN=''
-for sampl in s.sampleTypeList:
-    if inputDir.count(sampl):
-        currentSample=sampl
-        break;
+if len(currentSampleList)>0:
+    #for sampl,slist in s.sampleMap.iteritems():
+    for sampl in s.sampleTypeList:
+        if inputDir.count(sampl):
+            currentSampleKey=sampl
+            currentSampleList=[sampl]
+            break;
 #s = VBFAnalysis.sample.sample(inputDir, args.currentVariation)
 #currentSample = s.getsampleType()
 #print inputDir
@@ -43,33 +50,34 @@ if containerName!="":
     runNumber = s.getrunNumber()
     subfileN = s.getsubfileN()
 
-jps.AthenaCommonFlags.AccessMode = "TreeAccess"              #Choose from TreeAccess,BranchAccess,ClassAccess,AthenaAccess,POOLAccess
-jps.AthenaCommonFlags.TreeName = currentSample+args.currentVariation   #form:"Z_strongNominal"                    #when using TreeAccess, must specify the input tree name 
-print currentSample+args.currentVariation
-jps.AthenaCommonFlags.HistOutputs = ["MYSTREAM:HF"+currentSample+args.currentVariation+doLowNom_str+runNumberS+"_"+str(subfileN)+".root"]  #register output files like this. MYSTREAM is used in the code
-
-if currentSample == "data":
-    isMC = False
-else:
-    isMC = True
-
-if not(args.oldInput):
-    athAlgSeq += CfgMgr.HFInputAlg("HFInputAlg",
-                                   currentVariation = args.currentVariation,
-                                   currentSample = currentSample,
-                                   isMC = isMC,
-                                   doLowNom = args.doLowNom,
-                                   isHigh = not args.isLow);
-else:
-    if args.currentSample == "physics_micro":
+for currentSample in currentSampleList:
+    jps.AthenaCommonFlags.AccessMode = "TreeAccess"              #Choose from TreeAccess,BranchAccess,ClassAccess,AthenaAccess,POOLAccess
+    jps.AthenaCommonFlags.TreeName = currentSample+args.currentVariation   #form:"Z_strongNominal"                    #when using TreeAccess, must specify the input tree name 
+    print currentSample+args.currentVariation
+    jps.AthenaCommonFlags.HistOutputs = ["MYSTREAM:HF"+currentSample+args.currentVariation+doLowNom_str+runNumberS+"_"+str(subfileN)+".root"]  #register output files like this. MYSTREAM is used in the code
+    
+    if currentSample == "data":
         isMC = False
-    athAlgSeq += CfgMgr.HFInputOldInputAlg("HFInputOldInputAlg",
-                                           currentVariation = args.currentVariation,
-                                           currentSample = currentSample,
-                                           isMC = isMC,
-                                           doLowNom = args.doLowNom,
-                                           isHigh = not args.isLow);
-
-
-include("AthAnalysisBaseComps/SuppressLogging.py")              #Optional include to suppress as much athena output as possible. Keep at bottom of joboptions so that it doesn't suppress the logging of the things you have configured above
+    else:
+        isMC = True
+    
+    if not(args.oldInput):
+        athAlgSeq += CfgMgr.HFInputAlg("HFInputAlg",
+                                       currentVariation = args.currentVariation,
+                                       currentSample = currentSample,
+                                       isMC = isMC,
+                                       doLowNom = args.doLowNom,
+                                       isHigh = not args.isLow);
+    else:
+        if args.currentSample == "physics_micro":
+            isMC = False
+        athAlgSeq += CfgMgr.HFInputOldInputAlg("HFInputOldInputAlg",
+                                               currentVariation = args.currentVariation,
+                                               currentSample = currentSample,
+                                               isMC = isMC,
+                                               doLowNom = args.doLowNom,
+                                               isHigh = not args.isLow);
+    
+    
+    include("AthAnalysisBaseComps/SuppressLogging.py")              #Optional include to suppress as much athena output as possible. Keep at bottom of joboptions so that it doesn't suppress the logging of the things you have configured above
 
