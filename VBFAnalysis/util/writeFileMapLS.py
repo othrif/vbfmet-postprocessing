38516cf3ca6cff765dd48bfb5b721f4266a357b6
Doug Schaefer <schae@uct3-s1.mwt2.org>
2018-10-08 10:43:17 -0500

    =add LS file for local pileups to build pickle file map

---
+++ VBFAnalysis/util/writeFileMapLS.py
@@ -0,0 +1,55 @@
+import ROOT
+import subprocess
+import pickle
+
+l = open('mad.txt','r')
+
+#for i in `cat /tmp/files.txt`; do rucio list-file-replicas --pfns --protocol root --rse MWT2_UC_LOCALGROUPDISK  $i/ ; done &> /tmp/all.txt
+myMap = {}
+n=0
+for ite in l:
+
+    i=ite.rstrip('\n')
+    print 'File:',i
+
+    stdout=None
+    returnCode = -10
+    #while returnCode!=0:
+    #proc = subprocess.Popen(['/bin/bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    #stdout,error = proc.communicate('rucio list-file-replicas --pfns --protocol root --rse MWT2_UC_LOCALGROUPDISK  '+i+'/')
+    proc = subprocess.Popen(['/bin/bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    stdout = proc.communicate('ls  '+i+'/*root*')
+    #stdout = proc.communicate('rucio list-file-replicas --pfns --protocol root --rse MWT2_UC_LOCALGROUPDISK  '+i+'/')
+    
+        #print stdout
+        #print 'Return note: ',proc.returncode
+        #returnCode=proc.returncode
+    #print "ERROR: ",error
+    files=[]
+    for f in stdout:
+        if f !=None:
+            print '   Files',f
+            allF = f.split('\n')
+            for a in allF:
+                if len(a.strip())>1:
+                    files+=[a.strip()]
+    ikey = i
+    if i.count('361515'):
+        ikey = 'user.othrif.v07.361515.Sherpa_221_NNPDF30NNLO_Wtaunu_MAXHTPTV70_140_BFilter.e5340_s3126_r9364_p3575_MiniNtuple.root'
+    elif i.count('361516'):
+        ikey = 'user.othrif.v07.361516.Sherpa_221_NNPDF30NNLO_Wtaunu_MAXHTPTV70_140_BFilter.e5340_s3126_r9364_p3575_MiniNtuple.root'
+    elif i.count('361517'):
+        ikey = 'user.othrif.v07.361517.Sherpa_221_NNPDF30NNLO_Wtaunu_MAXHTPTV70_140_BFilter.e5340_s3126_r9364_p3575_MiniNtuple.root'
+    elif i.count('361518'):
+        ikey = 'user.othrif.v07.361518.Sherpa_221_NNPDF30NNLO_Wtaunu_MAXHTPTV70_140_BFilter.e5340_s3126_r9364_p3575_MiniNtuple.root'
+    elif i.count('361519'):
+        ikey = 'user.othrif.v07.361519.Sherpa_221_NNPDF30NNLO_Wtaunu_MAXHTPTV70_140_BFilter.e5340_s3126_r9364_p3575_MiniNtuple.root'
+    myMap[ikey]=files
+    n+=1
+    #if n>5:
+    #    break
+print myMap
+
+
+pickle.dump( myMap, open( "myMap.p", "wb" ) )
+print 'done'
