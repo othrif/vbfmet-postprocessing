9d1d090589fa57380afe8449855acb65633deec8
Doug Schaefer <schae@uct3-s1.mwt2.org>
2018-11-05 10:41:02 -0600

    =add MC check for local downloads

--- README.md
+++ README.md
@@ -27,6 +27,16 @@ cd $TestArea/build
 acmSetup
 ```
 
+# create the input files
+# generating a map is the preferred option for listing files
+python VBFAnalysis/util/writeFileMap.py # for GRID files
+python VBFAnalysis/util/writeFileMapLS.py # copy in a text file with each input file per line. There is an input for text files from the GRID. These can be overwritten preferring local files
+# collect the pickle file to get the total event counts for normalization
+python -p source/VBFAnalysis/data/uchicagoFileMap_v15Loose.p -o fout_v15raw.root 
+
+# to check the number of raw. The RAW counts need to be saved. Add the -r 1 option.
+python -p source/VBFAnalysis/data/uchicagoFileMap_v15Loose.p -o fout_v15raw.root -r 1
+
 
 ## Run VBFAnalysisAlg ##
 This generates the micro ntuples.
--- VBFAnalysis/util/getN.py
+++ VBFAnalysis/util/getN.py
@@ -8,6 +8,7 @@ import sys
 
 parser = argparse.ArgumentParser( description = "get total Nevent to weight samples", add_help=True , fromfile_prefix_chars='@')
 parser.add_argument( "-l", "--list", type = str, dest = "filelist", default = "listslim", help = "list of files" )
+parser.add_argument( "-r", "--event-count", type = int, dest = "event_count", default = 2, help = "event count. 1= raw, 2= sum of event weights" )
 parser.add_argument( "-g", "--grid", action = "store_true", dest = "grid", default = False, help = "-g if files are on grid site")
 parser.add_argument( "-o", "--output", type = str, dest = "output", default = "f_out_total_v04.root", help = "output file name" )
 parser.add_argument( "-p", "--pickle", type = str, dest = "pickle", default = None, help = "pickle file list" )
@@ -53,7 +54,7 @@ if args.pickle==None:
                 print 'zombie file', filepath
                 continue
             h = f.Get("NumberEvents")
-            nevent += h.GetBinContent(2)
+            nevent += h.GetBinContent(args.event_count)
         h_total.Fill(dsid_string,nevent)
 else:
     # run pickle file list
@@ -78,7 +79,7 @@ else:
                 print 'ERROR - zombie file', filepath
                 continue
             h = f.Get("NumberEvents")
-            nevent += h.GetBinContent(2)
+            nevent += h.GetBinContent(args.event_count)
             print 'total events: ',nevent
             f.Close()
         h_total.Fill(dsid_string,nevent)
