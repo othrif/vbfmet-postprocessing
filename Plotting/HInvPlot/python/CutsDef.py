2a0495fbf3774832168d355f673544ca8b3466a4
Ben Rosser <bjr@sas.upenn.edu>
2019-02-19 17:39:29 -0500

    Add a new 'wcranti' anti-ID region for fake lepton estimation
    I still need to actually implement the right cuts, but the region
    at least exists.

--- Plotting/HInvPlot/python/CutsDef.py
+++ Plotting/HInvPlot/python/CutsDef.py
@@ -21,16 +21,16 @@ class BasicCuts:
 
       Optional parameters:
     """
-        
+
     def __init__(self, Analysis, Chan, SameSign=0):
 
         if Analysis != 'allmjj':
             raise NameError('BasicCuts - unknown analysis string: %s' %Analysis)
-        
+
         self.analysis = Analysis
         self.chan     = Chan
         self.SameSign = SameSign
-        
+
     def PadKey(self, key, val, pf=None, sf=None):
         if len(key) > 0: key += '_'
         if pf != None: key += pf
@@ -41,7 +41,7 @@ class BasicCuts:
     def GetSelKey(self, chan=None):
 
          key = ''
-    
+
          if self.analysis !=None: key = self.PadKey(key, self.analysis)
          if self.SameSign == 1:   key = self.PadKey(key, 'ss')
 
@@ -49,7 +49,7 @@ class BasicCuts:
              if      chan !=None: key = self.PadKey(key,      chan)
          else:
              if self.chan !=None: key = self.PadKey(key, self.chan)
-         
+
          return key
 
     def GetChan(self):
@@ -63,7 +63,7 @@ class BasicCuts:
 #-------------------------------------------------------------------------
 def getLepChannelCuts(basic_cuts):
 
-    cuts = [] 
+    cuts = []
 
     if   basic_cuts.chan == None or basic_cuts.chan == 'aa':  pass
     elif basic_cuts.chan == 'nn': cuts += [CutItem('CutChannel', 'chanFlavor==1')]
@@ -74,7 +74,7 @@ def getLepChannelCuts(basic_cuts):
     elif basic_cuts.chan == 'ep': cuts += [CutItem('CutChannel', 'chanFlavor==4')]
     elif basic_cuts.chan == 'e': cuts += [CutItem('CutChannel', 'chanFlavor==4 || chanFlavor==5')]
     elif basic_cuts.chan == 'u': cuts += [CutItem('CutChannel', 'chanFlavor==2 || chanFlavor==3')]
-    elif basic_cuts.chan == 'l': cuts += [CutItem('CutChannel', 'chanFlavor==2 || chanFlavor==3 || chanFlavor==4 || chanFlavor==5')]           
+    elif basic_cuts.chan == 'l': cuts += [CutItem('CutChannel', 'chanFlavor==2 || chanFlavor==3 || chanFlavor==4 || chanFlavor==5')]
     elif basic_cuts.chan == 'em': cuts += [CutItem('CutChannel', 'chanFlavor==5')]
     elif basic_cuts.chan == 'up': cuts += [CutItem('CutChannel', 'chanFlavor==2')]
     elif basic_cuts.chan == 'um': cuts += [CutItem('CutChannel', 'chanFlavor==3')]
@@ -104,21 +104,21 @@ def ExtraCuts(options, n_mu=0, n_el=0, isEMu=False):
 
     if options.r207Ana:
         if isEMu:
-            cuts += [CutItem('CutBaseLep','n_siglep == 2')]                
+            cuts += [CutItem('CutBaseLep','n_siglep == 2')]
         elif n_mu>=0 and n_el>=0:
-            cuts += [CutItem('CutBaseLep','n_siglep == %s' %(n_mu))]        
+            cuts += [CutItem('CutBaseLep','n_siglep == %s' %(n_mu))]
         elif n_mu>=0 or n_el>=0:
             cuts += [CutItem('CutBaseMu','n_mu == %s' %(n_mu))]
             cuts += [CutItem('CutBaseEl','n_el == %s' %(n_el))]
         else:
             cuts += [CutItem('CutBaseLep','n_baselep == 0')]
         return cuts
-    #cuts += [CutItem('CutTruthFilter','TruthFilter < 0.5')]    
+    #cuts += [CutItem('CutTruthFilter','TruthFilter < 0.5')]
     #return cuts
     if isEMu:
-        cuts += [CutItem('CutBaseLep','n_baselep == 2')]                
+        cuts += [CutItem('CutBaseLep','n_baselep == 2')]
     elif n_mu>=0 and n_el>=0:
-        cuts += [CutItem('CutBaseLep','n_baselep == %s' %(n_mu))]        
+        cuts += [CutItem('CutBaseLep','n_baselep == %s' %(n_mu))]
     elif n_mu>=0 or n_el>=0:
         cuts += [CutItem('CutBaseMu','n_basemu == %s' %(n_mu))]
         cuts += [CutItem('CutBaseEl','n_baseel == %s' %(n_el))]
@@ -145,11 +145,11 @@ def getJetCuts(options, isPh=False):
             return cuts
         else:
             # New Cuts
-            #cuts += [CutItem('CutNjetCen',  'n_jet_cenj == 0')]    
+            #cuts += [CutItem('CutNjetCen',  'n_jet_cenj == 0')]
             cuts  = [CutItem('CutNjet',  'n_jet > 1 && n_jet < 5')]
             cuts += [CutItem('CutMaxCentrality',  'maxCentrality <0.6')]
             cuts += [CutItem('CutMaxMj3_over_mjj',  'maxmj3_over_mjj <0.05')]
-            
+
             cuts += [CutItem('CutJ0Pt',  'jetPt0 > 80.0')]
             cuts += [CutItem('CutJ1Pt',  'jetPt1 > 50.0')]
             #cuts += [CutItem('CutJ0Eta',  'jetEta0 > 2.5 || jetEta0 < -2.5')]
@@ -158,12 +158,12 @@ def getJetCuts(options, isPh=False):
         cuts = [CutItem('CutNjet',  'n_jet == 2')]
         cuts += [CutItem('CutJ0Pt',  'jetPt0 > 50.0')]
         cuts += [CutItem('CutJ1Pt',  'jetPt1 > 35.0')]
-        
+
     return cuts
 
 #-------------------------------------------------------------------------
 def getVBFCuts(options, isLep=False):
-    
+
     cuts = [CutItem('CutDPhijj',   'jj_dphi < 1.8')]
     if not isLep:
         cuts += [CutItem('CutDPhiMetj0','met_tst_j1_dphi > 1.0')]
@@ -179,8 +179,8 @@ def getVBFCuts(options, isLep=False):
         cuts += [CutItem('CutDEtajj','jj_deta > 4.0')]
         #cuts += [CutItem('CutDEtajjV','jj_deta > 2.5')]
         cuts += [CutItem('CutMjj','jj_mass > 1000.0')]
-        #cuts += [CutItem('CutMjjv2','jj_mass < 1500.0')]    
-    
+        #cuts += [CutItem('CutMjjv2','jj_mass < 1500.0')]
+
     return cuts
 
 #-------------------------------------------------------------------------
@@ -192,11 +192,11 @@ def metCuts(options, isLep=False):
     if options.r207Ana:
         cutMET.AddCut(CutItem('HighMET', '%s > 180.0' %(met_choice)), 'OR')
         cuts = [cutMET]
-        cuts += [CutItem('CutMetCSTJet', 'met_cst_jet > 150.0')]        
+        cuts += [CutItem('CutMetCSTJet', 'met_cst_jet > 150.0')]
     else:
         cutMET.AddCut(CutItem('HighMET', '%s > 180.0' %(met_choice)), 'OR')
         cutMET.AddCut(CutItem('LowMET', '%s > 150.0 && j0fjvt < 0.2 && j1fjvt < 0.2' %(met_choice)), 'OR')
-        cuts = [cutMET]    
+        cuts = [cutMET]
         #cuts += [CutItem('CutMetLow',       '%s > 100.0' %(options.met_choice))]
         cuts += [CutItem('CutMetCSTJet', 'met_cst_jet > 120.0')]
     return cuts
@@ -225,7 +225,7 @@ def getSRCuts(cut = '', options=None, basic_cuts=None, ignore_met=False, syst='N
 
     # add the extra cuts
     cuts += ExtraCuts(options)
-    
+
     if cut == 'BeforeMET':
         return GetCuts(cuts)
     if not ignore_met:
@@ -249,14 +249,14 @@ def getGamSRCuts(cut = '', options=None, basic_cuts=None, ignore_met=False):
 
     # add the extra cuts
     cuts += ExtraCuts(options)
-    
+
     if cut == 'BeforeMET':
         return GetCuts(cuts)
     if not ignore_met:
         cuts += [CutItem('CutMet',       '%s > 150.0' %(options.met_choice))]
         #cuts += [CutItem('CutMetLow',       '%s > 100.0' %(options.met_choice))]
         #cuts += [CutItem('CutMetCSTJet', 'met_cst_jet > 150.0')]
-        
+
     cuts += [CutItem('CutDPhiMetPh','met_tst_ph_dphi > 1.8')]
     cuts += [CutItem('CutPhCentrality','phcentrality > 0.4')]
     # VBF cuts
@@ -301,17 +301,17 @@ def getZCRCuts(cut = '', options=None, basic_cuts=None, ignore_met=False):
     if basic_cuts.chan=='eu':
         n_mu=1; n_el=1; isEMu=True;
     cuts += ExtraCuts(options, n_mu,n_el,isEMu)
-    
+
     if cut == 'BeforeMET':
-        return GetCuts(cuts)    
-    if not ignore_met:     
+        return GetCuts(cuts)
+    if not ignore_met:
         cuts += metCuts(options, True)
 
     if basic_cuts.chan=='ee':
-        cuts += [CutItem('CutLepVeto',   'n_mu == 0')]        
+        cuts += [CutItem('CutLepVeto',   'n_mu == 0')]
     if basic_cuts.chan=='uu':
-        cuts += [CutItem('CutLepVeto',   'n_el == 0')]        
-        
+        cuts += [CutItem('CutLepVeto',   'n_el == 0')]
+
     # VBF cuts
     cuts+=getVBFCuts(options, isLep=True)
 
@@ -330,9 +330,9 @@ def getWCRCuts(cut = '', options=None, basic_cuts=None, ignore_met=False, do_met
 
     # add the extra cuts
     cuts += ExtraCuts(options, 1,1)
-    
+
     if cut == 'BeforeMET':
-        return GetCuts(cuts)    
+        return GetCuts(cuts)
     if not ignore_met:
         cuts += metCuts(options, True)
     if do_met_signif:
@@ -341,6 +341,9 @@ def getWCRCuts(cut = '', options=None, basic_cuts=None, ignore_met=False, do_met
     cuts+=getVBFCuts(options, isLep=True)
 
     return GetCuts(cuts)
+
+def getWCRAntiIDCuts(cut = '', options=None, basic_cuts=None, ignore_met=False):
+    return getWCRCuts(cut, options, basic_cuts, ignore_met)
 
 #-------------------------------------------------------------------------
 def GetCutsObject(reg,cuts,options,alg_name):
@@ -361,22 +364,22 @@ def GetCuts(cuts):
 
     for cut in cuts:
         res += [cut]
-        
+
         if cut == cut.GetCutName():
             break
 
     return res
-    
+
 #-------------------------------------------------------------------------
 def fillSampleList(reg=None, key=None,options=None, basic_cuts=None):
-    
+
     sigs = {}
     sigs['higgs'] = ['hggf','hvh','hvbf']
     sigs['hggf']  = ['hggf']
-    #sigs['whww']  = ['whww']    
+    #sigs['whww']  = ['whww']
     sigs['hvh']   = ['hvh']
     sigs['hvbf']  = ['hvbf']
-    
+
     bkgs = {}
 
     bkgs['wqcd'] = ['wqcd']
@@ -385,22 +388,22 @@ def fillSampleList(reg=None, key=None,options=None, basic_cuts=None):
     bkgs['zewk'] = ['zewk']
     #bkgs['top1'] = ['top1']
     bkgs['top2'] = ['top2']
-    bkgs['vvv']  = ['vvv']    
-    #bkgs['zldy'] = ['zldy']    
-    bkgs['mqcd'] = ['mqcd']    
+    bkgs['vvv']  = ['vvv']
+    #bkgs['zldy'] = ['zldy']
+    bkgs['mqcd'] = ['mqcd']
     #bkgs['tall'] = ['top2','top1']
-    #bkgs['tall'] = ['top2','top1']    
+    #bkgs['tall'] = ['top2','top1']
 
     other={}
     other['dqcd']    = ['dqcd']
-    other['zqcdMad'] = ['zqcdMad']        
-    other['wqcdMad'] = ['wqcdMad']        
-    other['zqcdPow'] = ['zqcdPow']    
-    
+    other['zqcdMad'] = ['zqcdMad']
+    other['wqcdMad'] = ['wqcdMad']
+    other['zqcdPow'] = ['zqcdPow']
+
     samples = {}
     samples.update(sigs)
     samples.update(bkgs)
-    samples.update(other)    
+    samples.update(other)
 
     samples['data'] = ['data']
     samples['bkgs'] = []
@@ -433,7 +436,7 @@ def _fillPassEventRegistry(reg, cuts, options, basic_cuts=None, *arguments, **ke
     reg.SetVal('PassEvent::Cuts',     cuts_val)
     reg.SetVal('PassEvent::WriteAll', 'yes')
     if options.prec:
-        reg.SetVal('PassEvent::Precision',11)    
+        reg.SetVal('PassEvent::Precision',11)
 
     if options.print_raw:
         reg.SetVal('PassEvent::PrintRaw', 'yes')
@@ -509,6 +512,13 @@ def preparePassEventForWCR(alg_name, options, basic_cuts, cut='BASIC', do_met_si
 
     return ExecBase(alg_name, 'PassEvent', ROOT.Msl.PassEvent(), reg)
 
+def preparePassEventForWCRAntiID(alg_name, options, basic_cuts, cut='BASIC'):
+    """ Prepare events for the anti-ID version of the WCR."""
+    reg  = ROOT.Msl.Registry()
+    cuts = getWCRAntiIDCuts(cut, options, basic_cuts, ignore_met=options.ignore_met)
+    _fillPassEventRegistry(reg, cuts, options, basic_cuts, Debug='no', Print='no')
+    return ExecBase(alg_name, 'PassEvent', ROOT.Msl.PassEvent(), reg)
+
 #-------------------------------------------------------------------------
 def prepareFillEvent(alg_name, options):
 
@@ -526,24 +536,24 @@ def preparePlotEvent(alg_name, syst_name, *arguments, **keywords):
 
     alg = ROOT.Msl.PlotEvent()
     reg = ROOT.Msl.Registry()
-    
+
     reg.SetVal('PlotEvent::Debug',       'no')
     reg.SetVal('PlotEvent::Print',       'no')
     reg.SetVal('PlotEvent::VarPref',     'var_')
     reg.SetVal('PlotEvent::DetailLvl',   1)
-    #reg.SetVal('PlotEvent::Debug',       'yes')    
-    reg.SetVal('PlotEvent::VarVec',      ','.join((get_vars.GetPltStr(0,syst_name)))) 
+    #reg.SetVal('PlotEvent::Debug',       'yes')
+    reg.SetVal('PlotEvent::VarVec',      ','.join((get_vars.GetPltStr(0,syst_name))))
     reg.SetVal('PlotEvent::NBinVec',     ','.join((get_vars.GetPltStr(1,syst_name))))
     reg.SetVal('PlotEvent::LoVec',       ','.join((get_vars.GetPltStr(2,syst_name))))
     reg.SetVal('PlotEvent::HiVec',       ','.join((get_vars.GetPltStr(3,syst_name))))
-    
+
     pass_algs = []
-    
+
     for key, var in keywords.iteritems():
         if type(var) == type([]):
             reg.SetVal('PlotEvent::%s' %key, ','.join(var))
         elif key == 'PassAlg':
-            if var != None:            
+            if var != None:
                 pass_algs += [var]
         else:
             if key != None and var != None:
