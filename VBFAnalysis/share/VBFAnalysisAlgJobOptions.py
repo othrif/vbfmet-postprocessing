12d36d7e3af5120bc81e280683ce81ff9367d142
Othmane Rifki <othmane.rifki@cern.ch>
2019-03-01 18:26:01 +0100

    Add theory uncertainties output with --theoVariation

--- VBFAnalysis/share/VBFAnalysisAlgJobOptions.py
+++ VBFAnalysis/share/VBFAnalysisAlgJobOptions.py
@@ -10,7 +10,7 @@ from glob import glob
 #---- Options you could specify on command line -----
 jps.AthenaCommonFlags.FilesInput = glob(vars().get("input", "/eos/user/r/rzou/v04/user.othrif.v04.364106.Sherpa_221_NNPDF30NNLO_Zmumu_MAXHTPTV140_280_CVetoBVeto.e5271_s3126_r9364_r9315_p3575_MiniNtuple.root/*"))
 jps.AthenaCommonFlags.TreeName = "MiniNtuple"
-jps.AthenaCommonFlags.EvtMax =vars().get("nEvents", -1)                          #set on command-line with: --evtMax=-1 
+jps.AthenaCommonFlags.EvtMax =vars().get("nEvents", -1)                          #set on command-line with: --evtMax=-1
 #jps.AthenaCommonFlags.SkipEvents=0                       #set on command-line with: --skipEvents=0
 
 config = JobConfigurationBase("JobOptions")
@@ -19,6 +19,7 @@ arg_group.add_argument("--currentVariation", dest='currentVariation', default="N
 arg_group.add_argument("--normFile", dest='normFile', default="current.root", help="file with the total number of event processed")
 arg_group.add_argument("--containerName", dest='containerName', default="", help="container name used to look up the sample ID if not in the file path")
 arg_group.add_argument("--UseExtMC", dest="UseExtMC", action="store_true",default=False,help="Use extended MC samples")
+arg_group.add_argument("--theoVariation", dest='theoVariation', action="store_true", default=False, help="do theory systematic variations, default: False")
 
 # parse the commandline options
 args = config.parse_args()
@@ -37,11 +38,13 @@ if containerName!="":
     currentSample = s.getsampleType()
     isMC = s.getisMC()
     runNumber = s.getrunNumber()
-    subfileN = s.getsubfileN()    
+    subfileN = s.getsubfileN()
 
 print inputDir, " ", currentSample, " ", str(runNumber)
 jps.AthenaCommonFlags.HistOutputs = ["MYSTREAM:"+currentSample+args.currentVariation+str(runNumber)+subfileN+".root"]  #optional, register output files like this. MYSTREAM is used in the code
-
+isSherpaVjets = runNumber in range(308092, 308098+1) or runNumber in range(364100, 364197+1) or runNumber in range(309662, 309679+1)
+if args.theoVariation and args.currentVariation == "Nominal" and isSherpaVjets:
+  jps.AthenaCommonFlags.HistOutputs = ["ANALYSIS:theoVariation_"+currentSample+args.currentVariation+str(runNumber)+subfileN+".root"]
 athAlgSeq += CfgMgr.VBFAnalysisAlg("VBFAnalysisAlg",
                                    currentVariation = args.currentVariation,
                                    normFile = args.normFile,
@@ -51,6 +54,8 @@ athAlgSeq += CfgMgr.VBFAnalysisAlg("VBFAnalysisAlg",
                                    ExtraVars=True,
                                    UseExtMC=args.UseExtMC,
                                    QGTagger=False,
-                                   runNumberInput = runNumber);
+                                   runNumberInput = runNumber,
+                                   theoVariation = args.theoVariation and isSherpaVjets
+                                   );
 
 include("AthAnalysisBaseComps/SuppressLogging.py") #optional line
