b5c609d6f1b0e84f325abd1e3d24e7220c8e33a6
Amanda Steinhebel <amanda.lynn.steinhebel@cern.ch>
2019-03-18 01:53:33 -0700

    Script to run drawStack

---
+++ Plotting/rundrawStack.sh
@@ -0,0 +1,9 @@
+out=$1
+reg=$2
+var=$3
+
+key1='pass_'
+key2='_Nominal'
+
+python HInvPlot/macros/drawStack.py $out --vars $var --selkey "$key1$reg$key2" --wait --do-ratio --atlas-style /export/home/asteinhe/atlasstyle/ --blind
+
