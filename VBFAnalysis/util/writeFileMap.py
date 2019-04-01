0330c10ea3e9584066eb0158a5687b834be24a6a
Doug Schaefer <schae@uct3-s1.mwt2.org>
2019-04-01 06:59:33 -0500

    update dilepton trigger

--- VBFAnalysis/util/writeFileMap.py
+++ VBFAnalysis/util/writeFileMap.py
@@ -5,7 +5,7 @@ import sys
 #l = open('v21Loose.txt','r')
 #l = open('v26bLoose_rui_sort.txt','r')
 #l = open('v28Loose.txt','r')
-l = open('vbf.txt','r')
+l = open('v26Loose_pflow.txt','r')
 
 #for i in `cat /tmp/files.txt`; do rucio list-file-replicas --pfns --protocol root --rse MWT2_UC_LOCALGROUPDISK  $i/ ; done &> /tmp/all.txt
 myMap = {}
