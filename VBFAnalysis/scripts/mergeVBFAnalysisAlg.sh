5ca99dbfe75b84ed0085fb3257536fe554a4770c
Doug Schaefer <schae@uct3-s3.mwt2.org>
2020-01-08 07:20:33 -0600

    add

--- VBFAnalysis/scripts/mergeVBFAnalysisAlg.sh
+++ VBFAnalysis/scripts/mergeVBFAnalysisAlg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash                                                                                                                                                                                                       
 #OUTDIRM=/share/t3data2/schae/v32ETight
 #OUTDIRM=${1:-v34mc16a}
-OUTDIRM=v34ELooseSYSTJan7
+OUTDIRM=v34ALooseSYSTJan8
 mkdir $OUTDIRM
 hadd $OUTDIRM/data.root data*root
 #hadd $OUTDIRM/VVV.root VVV*root
