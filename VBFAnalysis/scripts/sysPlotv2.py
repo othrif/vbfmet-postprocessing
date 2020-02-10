26cc83ecdbe4a1d180d09578e0f0383108d0780a
schaed <schae@cern.ch>
2020-02-10 12:11:14 +0100

    merge the w and z for jet and met systematics

--- VBFAnalysis/scripts/sysPlotv2.py
+++ VBFAnalysis/scripts/sysPlotv2.py
@@ -1,10 +1,11 @@
 import ROOT
-import sys
+import sys,os
 import math
 import VBFAnalysis.ATLAS as ATLAS
 import VBFAnalysis.Style as Style
 from optparse import OptionParser
 import HInvPlot.systematics as vbf_syst
+import HInvPlot.JobOptions as config
 
 def HistName(histName, regionName, systName, binNum):
     return "h"+histName+"_"+(regionName.replace('X','%s' %binNum)).replace('Nom_',systName+'_')
@@ -26,6 +27,20 @@ def DeclareCanvas(options):
         can.cd(1)
     return can
 
+def DrawRatio(options,can,nom, varHist=[]):
+    color=1
+    drawOpt=''
+    for v in varHist:
+        v.Divide(nom)
+        v.GetYaxis().SetRangeUser(0.9,1.1)
+        v.SetLineColor(color)
+        color+=1
+        v.Draw(drawOpt)
+        drawOpt='same'
+
+    can.Update()
+    can.WaitPrimitive()
+    
 def GetLegLabel(options,can):
 
     #ROOT.gPad.BuildLegend(0.7,0.7,0.9,0.9)
@@ -35,8 +50,100 @@ def GetLegLabel(options,can):
     for text in texts:
         text.Draw()
 
+def PrintPulls(rfile,options,can,histName,regions):
+    print 'Reading file: ',options.pullsFile
+    if not os.path.exists(options.pullsFile):
+        print 'file does not exist'
+    pullMap={}
+    pullList = open(options.pullsFile)
+    systFile=open('../listTheorySyst11STv4')
+    systMap={}
+    key=''
+    for l in systFile:
+        if l.count('SRup'):
+            key='SRup'
+            continue
+        elif l.count('SRdown'):
+            key='SRdown'
+            continue
+        elif l.count('ZCRdown'):
+            key='ZCRdown'
+            continue
+        elif l.count('ZCRup'):
+            key='ZCRup'
+            continue
+        elif l.count('WCRdown'):
+            key='WCRdown'
+            continue        
+        elif l.count('WCRup'):
+            key='WCRup'
+            continue
+        if not l.strip():
+            continue
+        sLine=l.strip().split(' ')
+        if key not in systMap:
+            systMap[key]={}
+        #print sLine[0],sLine[1]
+        systMap[key][sLine[0]+"_bin1"]=sLine[1].rstrip('\n').split(',')
+        
+    for i in pullList:
+        myList= i.strip().rstrip('\n').split(' ')
+        myList=filter(None, myList)
+        #print myList
+        if myList[0].count('alpha_'):
+            pullMap[myList[0].replace('alpha_','')]=[float(myList[2]),float(myList[4])]
+    for r in regions:
+        rkey='SR'
+        if r.count('two'):
+            rkey='ZCR'
+        if r.count('one'):
+            rkey='WCR'
+        #for ibin in range(1,options.binNum+1):
+        for ibin in [4,9,5,10]:
+            print 'Bin: ',ibin
+            nomBinH=rfile.Get(HistName(histName, r, 'Nom', ibin))
+            nomVal=nomBinH.GetBinContent(1)
+            if not nomBinH:
+                print 'could not load Nominal: ',HistName(histName, r, 'Nom', ibin)
+                continue            
+            for systName,pulls in pullMap.iteritems():
+                pullValue = pulls[0]/pulls[1]
+                sysBinH=rfile.Get(HistName(histName, r, systName+'High', ibin))
+                #print systName #,systMap[rkey+'up']
+                #print systName+'_bin1'
+                if (systName) in systMap[rkey+'up']:
+                    #print 'FOUND:',systName
+                    if not systName.count(histName):
+                        continue
+                    upPull=pullValue*(nomVal)*(float(systMap[rkey+'up'][systName][ibin-1])-1.0)
+                    dwPull=pullValue*(nomVal)*(float(systMap[rkey+'down'][systName][ibin-1])-1.0)                    
+                    if abs(dwPull)>0.01 or  abs(upPull)>0.01:
+                        print '%0.2f %0.2f %s %s THEORY %s bin %s' %(upPull, dwPull, systName,histName, r,ibin)
+                if not sysBinH:
+                    #print 'could not load Syst: ',HistName(histName, r, systName+'High', ibin)
+                    continue
+                sysBinL=rfile.Get(HistName(histName, r, systName+'Low', ibin))
+                if not sysBinL:
+                    #print 'could not load Syst: ',HistName(histName, r, systName+'Low', ibin)
+                    continue
+                upPull=pullValue*(nomVal-sysBinH.GetBinContent(1))
+                dwPull=pullValue*(nomVal-sysBinL.GetBinContent(1))
+                if abs(dwPull)>0.01 or  abs(upPull)>0.01:
+                    if pullValue>0.0:
+                        print '%0.2f + %0.2f %s %s' %(nomVal,upPull,systName,histName)
+                    else:
+                        print '%0.2f - %0.2f %s %s' %(nomVal,dwPull,systName,histName)                        
+    pullList.close()
+    
 def Smooth(rfile,options,can,systName,histName,regions):
 
+    smoothSyle=1
+    if systName.count('EL_'):
+        smoothSyle=1
+    elif systName.count('JET_'):
+        smoothSyle=3
+    elif systName.count('MET_'):
+        smoothSyle=3
     nomMap={}
     sysUpMap={}
     sysDwMap={}
@@ -75,18 +182,16 @@ def Smooth(rfile,options,can,systName,histName,regions):
         sysDwMap[r]=hsysdw.Clone()
 
     # takes the integral in the region. Then scales by the integral over all regions
-    if options.smooth==1:
+    updateHist=[]
+    #rNewfile=ROOT.TFile(options.input,'UPDATE')
+    rNewfile=ROOT.TFile('/tmp/HFALL_feb5_sysUPDATE.root','UPDATE')
+    if options.smooth==1 or (options.smooth==5 and smoothSyle==1):
         print 'smoothing option 1'
-        updateHist=[]
-        #rNewfile=ROOT.TFile(options.input,'UPDATE')
-        rNewfile=ROOT.TFile('/tmp/HFALL_feb5_sysUPDATE.root','UPDATE')
         for r in regions:
-            #print r
             nomInt = nomMap[r].Integral(1,options.binNum)
             upInt  = sysUpMap[r].Integral(1,options.binNum)
             dwInt  = sysDwMap[r].Integral(1,options.binNum)
             for ibin in range(1,options.binNum+1):
-                #currentNomV=nomMap[r].GetBinContent(binOrder[ibin-1])
                 currentNomV=nomMap[r].GetBinContent(binOrder[ibin-1])
                 if upInt!=0.0 and nomInt!=0.0:
                     sysBinH=rNewfile.Get(HistName(histName, r, systName+'High', ibin))
@@ -104,12 +209,84 @@ def Smooth(rfile,options,can,systName,histName,regions):
                     #sysBinH.Scale(upInt/nomInt)
                     sysBinH.SetBinContent(1,dwInt/nomInt*currentNomV)
                     updateHist+=[sysBinH]
-        for ha in updateHist:
-            #print ha.GetName()
-            rNewfile.cd()
-            ha.Write(ha.GetName(),ROOT.TObject.kOverwrite)
-        #rNewfile.Write("",ROOT.TObject.kOverwrite);
-        rNewfile.Close()
+    elif options.smooth==2 or (options.smooth==5 and smoothSyle==2):
+        print 'smoothing option 2 - parabolic smoothing'
+        smooth_tool=ROOT.SmoothHist()
+        smooth_tool.setNmax(1)
+        for r in regions:
+            sysBefore=sysUpMap[r].Clone()
+            hSmoothed = smooth_tool.smoothHistogram(nomMap[r], sysUpMap[r], True)
+            smooth_tool.smoothHistogram(nomMap[r], sysDwMap[r], True)            
+
+            if options.wait:
+                rhSmoothed = hSmoothed.Clone()                
+                DrawRatio(options,can,nomMap[r], varHist=[sysBefore,rhSmoothed])
+            for ibin in range(1,options.binNum+1):
+                sysBinH=rNewfile.Get(HistName(histName, r, systName+'High', ibin))
+                if not sysBinH:
+                    continue
+                sysBinH.SetBinContent(1,sysUpMap[r].GetBinContent(binOrder[ibin-1]))
+                updateHist+=[sysBinH]
+                sysBinH=rNewfile.Get(HistName(histName, r, systName+'Low', ibin))
+                if not sysBinH:
+                    continue
+                sysBinH.SetBinContent(1,sysDwMap[r].GetBinContent(binOrder[ibin-1]))
+                updateHist+=[sysBinH]
+    elif options.smooth==3 or (options.smooth==5 and smoothSyle==3):
+        print 'smoothing option 3 - combine regions'
+        smooth_tool=ROOT.SmoothHist()
+        smooth_tool.setNmax(1)
+        
+        # first add the regions
+        CombineSysNomMap={}
+        CombineSysUpMap={}
+        CombineSysDwMap={}
+        for r in regions:
+            CombineSysNomMap[r]= nomMap[r].Clone()
+            CombineSysUpMap[r] = sysUpMap[r].Clone()
+            CombineSysDwMap[r] = sysDwMap[r].Clone()            
+            for r2 in regions:
+                if r==r2:
+                    continue
+                if r.count('two') and r2.count('two'):
+                    CombineSysNomMap[r].Add( nomMap[r2])
+                    CombineSysUpMap[r]. Add( sysUpMap[r2])
+                    CombineSysDwMap[r]. Add( sysDwMap[r2])
+                if r.count('one') and r2.count('one'):
+                    CombineSysNomMap[r].Add( nomMap[r2])
+                    CombineSysUpMap[r]. Add( sysUpMap[r2])
+                    CombineSysDwMap[r]. Add( sysDwMap[r2])
+                    
+        # smooth with the added regions
+        for r in regions:
+            sysBefore=sysUpMap[r].Clone()
+            hSmoothed = smooth_tool.smoothHistogram(CombineSysNomMap[r], CombineSysUpMap[r], True)
+            smooth_tool.smoothHistogram(CombineSysNomMap[r], CombineSysDwMap[r], True)            
+
+            if options.wait:
+                rhSmoothed = hSmoothed.Clone()                
+                DrawRatio(options,can,nomMap[r], varHist=[sysBefore,rhSmoothed])
+            for ibin in range(1,options.binNum+1):
+                sysBinH=rNewfile.Get(HistName(histName, r, systName+'High', ibin))
+                if not sysBinH:
+                    continue
+                upV=sysBinH.GetBinContent(1)
+                if upV!=0.0:
+                    sysBinH.SetBinContent(1,upV*CombineSysUpMap[r].GetBinContent(binOrder[ibin-1])/CombineSysNomMap[r].GetBinContent(binOrder[ibin-1]))
+                updateHist+=[sysBinH]
+                sysBinH=rNewfile.Get(HistName(histName, r, systName+'Low', ibin))
+                if not sysBinH:
+                    continue
+                dwV=sysBinH.GetBinContent(1)
+                if dwV!=0.0:
+                    sysBinH.SetBinContent(1,dwV*CombineSysDwMap[r].GetBinContent(binOrder[ibin-1])/CombineSysNomMap[r].GetBinContent(binOrder[ibin-1]))
+                updateHist+=[sysBinH] 
+                    
+    # write the updated histograms
+    for ha in updateHist:
+        rNewfile.cd()
+        ha.Write(ha.GetName(),ROOT.TObject.kOverwrite)
+    rNewfile.Close()
         
 def DrawRatio(rfile,options,can,systName,histName,regions):
 
@@ -263,6 +440,7 @@ if __name__=='__main__':
     p.add_option('--nBin', type='int', default=1, help='Defines which bin is plotted')
     p.add_option('--smooth', type='int', default=0, help='Smooth options: 1 average bins')    
     p.add_option('-s', '--syst', type='string', default="All", help='NEEDS FIXING. defines the systematics that are plotted. -s all <- will plot all available systematics. Otherwise give a key to the dict in systematics.py')# FIXME
+    p.add_option('--pullsFile', type='string', default=None, help='pulls file, print yields')
     p.add_option('-d', '--data', action='store_true', help='Draw data')
     p.add_option('--unBlindSR', action='store_true', help='Unblinds the SR bins')
     p.add_option('--debug', action='store_true', help='Print in debug mode')    
@@ -291,30 +469,39 @@ if __name__=='__main__':
     ]
     if options.batch:
         ROOT.gROOT.SetBatch(True)
+    # Load libraries
+    config.loadLibs(ROOT)
     can=DeclareCanvas(options)
-    openOpt='READ'
-    #if options.smooth>0:
-    #    openOpt='UPDATE'
-    rfile=ROOT.TFile(options.input,openOpt)
+    rfile=ROOT.TFile(options.input,'READ')
+    # read in pulls file and print the yields
+    if options.pullsFile!=None:
+        #for histName in histNames:
+        for histName in ["Z_strong", "Z_EWK", "ttbar"]:
+            PrintPulls(rfile,options,can,histName,['VBFjetSel_XNom_twoEleCRX_obs_cuts','VBFjetSel_XNom_twoMuCRX_obs_cuts'])
+        sys.exit(0)
+        
+    # which syst
     systName='EL_EFF_Trigger_TOTAL_1NPCOR_PLUS_UNCOR'
-    systToSmooth=[#'EL_EFF_Trigger_TOTAL_1NPCOR_PLUS_UNCOR',
-                      #'JET_fJvtEfficiency','JET_JvtEfficiency','JET_JvtEfficiency',
-                      #'JET_Pileup_OffsetMu',
-                      #'JET_Pileup_OffsetNPV',
-                      #'JET_Pileup_PtTerm',
-                      #'JET_Pileup_RhoTopology',
-                      #'JET_EffectiveNP_Modelling2',
-                      #'JET_EffectiveNP_Modelling1',
-                      #'JET_EffectiveNP_Modelling3',
+    systToSmooth=['EL_EFF_Trigger_TOTAL_1NPCOR_PLUS_UNCOR',
+                      'EL_EFF_Iso_TOTAL_1NPCOR_PLUS_UNCOR',
+                      'JET_fJvtEfficiency','JET_JvtEfficiency','JET_JvtEfficiency',
+                      'JET_Pileup_OffsetMu',
+                      'JET_Pileup_OffsetNPV', # still looks weird. some regions are higher or lower than others
+                      'JET_Pileup_PtTerm',
+                      'JET_Pileup_RhoTopology',
+                      'JET_EffectiveNP_Modelling2',
+                      'JET_EffectiveNP_Modelling1',
+                      'JET_EffectiveNP_Modelling3',
                       'JET_Flavor_Composition',
-                      #'JET_JER_DataVsMC_MC16',
-                      #'JET_JER_EffectiveNP_1',
-                      #'JET_JER_EffectiveNP_2',
-                      #'JET_JER_EffectiveNP_3',
-                      #'JET_JER_EffectiveNP_4',
+                      'JET_JER_DataVsMC_MC16',
+                      'JET_JER_EffectiveNP_1',
+                      'JET_JER_EffectiveNP_2',
+                      'JET_JER_EffectiveNP_3',
+                      'JET_JER_EffectiveNP_4',
                       'MET_SoftTrk_Scale',
                       'JET_Flavor_Response',
                       'JET_EtaIntercalibration_TotalStat',
+                      #'PRW_DATASF', # amanda suggested
                       ]
     allSyst=[]
     if options.syst=='All':
