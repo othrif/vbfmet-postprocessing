cb9df72e9ebb6ce76d94e0e055554f09d7a5d9c6
schaed <schae@cern.ch>
2020-06-08 10:18:22 +0200

    add high dphijj for fitting to HF

--- VBFAnalysis/src/HFInputAlg.h
+++ VBFAnalysis/src/HFInputAlg.h
@@ -61,8 +61,10 @@ class HFInputAlg: public ::AthAnalysisAlgorithm {
   Bool_t doPlot = false;
   Bool_t singleHist = false;
   Bool_t doVBFMETGam = false;
+  Bool_t doMTFit = false;  
   Bool_t doDuplicateCheck = false;
   Bool_t doDoubleRatio = false;
+  Bool_t doHighDphijj = false;  
   bool is2015;
   bool is2016;
   TTree *m_tree = 0;
